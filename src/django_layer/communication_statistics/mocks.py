d871cb1d0a11a9e3218df5511c48037459ba40a8
Gertranger <fahford@gmail.com>
2024-03-26 15:50:14 +0100

    Исправлены моки

--- src/django_layer/communication_statistics/mocks.py
+++ src/django_layer/communication_statistics/mocks.py
@@ -9,52 +9,52 @@ MOCKED_STATISTICS = {
     },
     'departments': {
         'marketing': {
-            'mass': 100,
-            'personal': 100,
-            'informational': 100,
-            'survey': 100,
-            'commercial_survey': 100,
-            'service': 100
+            'mass': 25,
+            'personal': 169,
+            'informational': 74,
+            'survey': 99,
+            'commercial_survey': 57,
+            'service': 144
         },
         'sales': {
-            'mass': 100,
-            'personal': 100,
-            'informational': 100,
-            'survey': 100,
-            'commercial_survey': 100,
-            'service': 100
+            'mass': 143,
+            'personal': 32,
+            'informational': 95,
+            'survey': 124,
+            'commercial_survey': 63,
+            'service': 97
         },
         'informational': {
-            'mass': 100,
-            'personal': 100,
-            'informational': 100,
-            'survey': 100,
-            'commercial_survey': 100,
-            'service': 100
+            'mass': 68,
+            'personal': 79,
+            'informational': 146,
+            'survey': 143,
+            'commercial_survey': 122,
+            'service': 96
         },
         'survey': {
-            'mass': 100,
-            'personal': 100,
-            'informational': 100,
-            'survey': 100,
-            'commercial_survey': 100,
-            'service': 100
+            'mass': 121,
+            'personal': 44,
+            'informational': 92,
+            'survey': 86,
+            'commercial_survey': 142,
+            'service': 162
         },
         'commercial_survey': {
-            'mass': 100,
-            'personal': 100,
-            'informational': 100,
-            'survey': 100,
-            'commercial_survey': 100,
-            'service': 100
+            'mass': 157,
+            'personal': 92,
+            'informational': 59,
+            'survey': 91,
+            'commercial_survey': 141,
+            'service': 37
         },
         'service': {
-            'mass': 100,
-            'personal': 100,
-            'informational': 100,
-            'survey': 100,
-            'commercial_survey': 100,
-            'service': 100
+            'mass': 86,
+            'personal': 184,
+            'informational': 67,
+            'survey': 57,
+            'commercial_survey': 75,
+            'service': 64
         }
     }
 }
